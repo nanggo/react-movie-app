2a514f16b5551cec76d47916c39f3b4fc18e060f
nanggo <yamsiri@gmail.com>
2020-01-31 00:02:35 +0900

    Modify about.js

--- src/routes/About.js
+++ src/routes/About.js
@@ -2,14 +2,15 @@ import React from "react";
 import "./About.css";
 
 function About(props) {
-    console.log(props)
+  console.log(props);
   return (
-      <div className="about__container">
-          <span>
-            "Freedom is the freedom to say that two plus tow  make four. If that ~~"
-          </span>
-          <span>- George Orwell, 1984</span>
-      </div>
+    <div className="about__container">
+      <span>
+        “Freedom is the freedom to say that two plus two make four. If that is
+        granted, all else follows.”
+      </span>
+      <span>− George Orwell, 1984</span>
+    </div>
   );
 }
 
